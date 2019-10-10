138aa608606fa0985937c9e392b5b8a0cd6a3b7a
Paul Bartsch <pdbartsch@gmail.com>
2019-10-10 15:21:46 -0700

    input types

--- parklandapp/forms.py
+++ parklandapp/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import Form
+from wtforms import Form, widgets
 from wtforms_components import IntegerField
 from wtforms.validators import NumberRange, DataRequired
 
-from wtforms.fields.html5 import TelField
+# from wtforms.fields.html5 import TelField
 
 
 # class MathQuizForm(FlaskForm):
@@ -12,5 +12,13 @@ from wtforms.fields.html5 import TelField
 #     )
 
 
+# class MathQuizForm(FlaskForm):
+#     user_answer = TelField("user_answer", validators=[DataRequired()])
+
+
 class MathQuizForm(FlaskForm):
-    user_answer = TelField("user_answer", validators=[DataRequired()])
+    user_answer = IntegerField(
+        "user_answer",
+        validators=[NumberRange(min=1, max=1000), DataRequired()],
+        widget=widgets.Input(input_type="tel"),
+    )
