12d5092280271f69333aecb02b3c2a55a12e0488
Paul Bartsch <pdbartsch@gmail.com>
2019-10-10 15:12:26 -0700

    changed to TelField for bigger numbers on mobile

--- parklandapp/forms.py
+++ parklandapp/forms.py
@@ -3,8 +3,14 @@ from wtforms import Form
 from wtforms_components import IntegerField
 from wtforms.validators import NumberRange, DataRequired
 
+from wtforms.fields.html5 import TelField
+
+
+# class MathQuizForm(FlaskForm):
+#     user_answer = IntegerField(
+#         "user_answer", validators=[NumberRange(min=1, max=1000), DataRequired()]
+#     )
+
 
 class MathQuizForm(FlaskForm):
-    user_answer = IntegerField(
-        "user_answer", validators=[NumberRange(min=1, max=1000), DataRequired()]
-    )
+    user_answer = TelField("user_answer", validators=[DataRequired()])
