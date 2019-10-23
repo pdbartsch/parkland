1c6decb74150b778b30b78d080fd830ffe8aa814
Paul Bartsch <BartschP@fis.ucsb.edu>
2019-10-23 15:09:52 -0700

    added running goal check in

--- parklandapp/__init__.py
+++ parklandapp/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
-application = app = Flask(__name__)
+application = app = Flask(__name__, instance_relative_config=True)
 
 app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
-
+app.config.from_pyfile("config.py")
 
 from parklandapp import routes
