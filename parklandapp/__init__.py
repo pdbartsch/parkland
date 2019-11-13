9b03619cb8e2e895cdcd50005ed85b82501c58e5
Paul Bartsch <BartschP@fis.ucsb.edu>
2019-11-13 09:15:15 -0800

    cleanup

--- parklandapp/__init__.py
+++ parklandapp/__init__.py
@@ -4,5 +4,6 @@ application = app = Flask(__name__, instance_relative_config=True)
 
 app.config["SECRET_KEY"] = "REDACTED-SECRET-KEY"
 app.config.from_pyfile("config.py")
+# app.config["SESSION_TYPE"] = "filesystem"
 
 from parklandapp import routes
